33a84ec4ddc492ec14b337a8ccd551aeacbb1d7d
TexugoGospel <riquepinheiro1@outlook.com>
2025-08-14 20:43:16 -0300

    Adiciona Personagem

---
+++ personagem.ts
@@ -0,0 +1,13 @@
+ export class personagem {
+    nome: string = "";
+    classe: string = "";
+    raca: string ="";
+    nivel: number = 0;
+    arma: string = "";
+    manaAtual: number = 0;
+    manaMaxima: number = 0;
+    vida: number = 0;
+    vidaAtual: number = 0;
+    vidaMaxima: number = 0;
+    poderAtaque: number =;
+ }
