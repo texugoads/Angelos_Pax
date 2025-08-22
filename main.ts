815263277340fde0816c7cef73935ddaa97bb535
HENRIQUE PEREIRA PINHEIRO <05949156005@senacrs.edu.br>
2025-08-21 21:48:46 -0300

    Adiciona construtor e método de ataque

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 import { personagem } from "./personagem";
+import prompt from "prompt-sync";
 
-const p: personagem = new personagem();
+const teclado = prompt();
+
+const p: personagem = new personagem("Edécio");
 p.nome = "Dedé";
 p.classe = "Monge";
 p.raca = "Lith";
@@ -12,4 +15,37 @@ p.vidaMaxima = 100;
 p.vidaAtual = p.vidaMaxima
 p.poderAtaque = 1;
 
+while(true){
+
+    
+    console.log("+-------------MENU--------+")
+    console.log("|1.Treinar Poder de Ataque|")
+    console.log("|2.Ver status             |")
+    console.log("|9. Sair                  |")
+    console.log("+-------------------------+")
+    console.log("Eu amo o David             ")
+    
+    const escolha: number = +teclado("escolha uma opçao do menu:")
+    
+    if(escolha == 9){
+        break;
+    }
+    switch (escolha) {
+        case 1: 
+        treinarPoderAtaque(p);
+        break;
+        
+        case 2:
+            console.table(p);
+            break;
+            
+            default:
+                break;
+                console.log("Opção inválida, tente novamente.");
+            }
+            
+}
+function treinarPoderAtaque(person: personagem): void {
+    person.treinarPoderAtaque();
+}
 console.table(p);
--- personagem.ts
+++ personagem.ts
@@ -1,12 +1,32 @@
  export class personagem {
-    nome: string = "";
-    classe: string = "";
-    raca: string ="";
-    nivel: number = 0;
-    arma: string = "";
-    manaAtual: number = 0;
-    manaMaxima: number = 0;
-    vidaAtual: number = 0;
-    vidaMaxima: number = 0;
-    poderAtaque: number =0;
+    nome: string ;
+    classe: string ;
+    raca: string ;
+    nivel: number; 
+    arma: string ;
+    manaAtual: number; 
+    manaMaxima: number; 
+    vidaAtual: number ;
+    vidaMaxima: number ;
+    poderAtaque: number ;
+
+
+    constructor(nome: string){
+      this.nome = nome;
+      this.classe = "";
+      this.raca = "";
+      this.nivel = 0;
+      this.arma = "";
+      this.manaAtual = 0;
+      this.manaMaxima = 0;
+      this.vidaAtual = 0;
+      this.vidaMaxima = 0;
+      this.poderAtaque = 0;
+
+    }
+ 
+ treinarPoderAtaque(): void {
+     this.poderAtaque += 3 + this.poderAtaque*1.1;
  }
+ 
+}
